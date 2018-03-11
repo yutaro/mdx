e8b87f609d2c1b4a171dc6f4a3b3a70615545c9d
yutaro <llgoodrr@gmail.com>
2018-03-11 21:47:40 +0900

    update :

--- src/js/router/routes.js
+++ src/js/router/routes.js
@@ -3,9 +3,11 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 import index from '../view/index.vue'
+import note  from '../view/note.vue'
 
 export default new VueRouter({
     routes : [
-        { path : "/" , component : index }
+        { path : "/" , component : index },
+        { path : '/note', component : note },
     ]
 })
---
+++ src/js/view/note.vue
@@ -0,0 +1,39 @@
+<template lang="pug">
+    div
+        markdown-editor( v-model="content" ref="markdownEditor" :configs="configs" )
+</template>
+
+<style>
+    @import '~simplemde/dist/simplemde.min.css';
+</style>
+
+
+<script>
+import Vue from 'vue';
+import markdownEditor from '../components/mde.vue';
+export default {
+    components : {
+        markdownEditor
+    },
+    data(){
+        return {
+            content : '',
+            configs : {
+                status : false,
+                spellChecker : false
+            }
+        }
+    },
+    computed : {
+        simplemde (){
+            return this.$refs.markdownEditor.simplemde
+        }
+    },
+    mounted(){
+        console.log(this.simplemde)
+        this.simplemde.togglePreview()
+
+
+    }
+}
+</script>
