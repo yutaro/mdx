f8d8aa8d20c366b81e0d43c01eeaab2cb36f76bf
yutaro <llgoodrr@gmail.com>
2018-03-13 10:29:21 +0900

    update : gituhb page

--- src/js/main.js
+++ src/js/main.js
@@ -5,16 +5,21 @@ Vue.use(Vuex)
 import Slide from './modules/slide.js'
 import Line  from './modules/line.js'
 
+import note from './view/note.vue';
+
 const store = new Vuex.Store({
     modules : {
 
     }
 })
 
-import router from './router/routes.js';
+// import router from './router/routes.js';
 
 new Vue({
     el : '#app',
     store,
-    router,
+    // router,
+    components : {
+        note
+    }
 })
